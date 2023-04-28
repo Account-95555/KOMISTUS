6fdc0e5f1646333d19fd3b8b427ba65676352cb9
me <acc95555@gmail.com>
2023-04-28 23:48:36 +0800

    Title Scene and Logo Scene created

---
+++ Assets/Scripts/SceneManagers/LogoSceneManager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class LogoSceneManager : SceneManagerCommon
+{
+    public GameObject videoHolder;
+    public VideoPlayer loadingVideo;
+    // Start is called before the first frame update
+    void Start()
+    {
+        loadingVideo = videoHolder.GetComponent<VideoPlayer>();
+        StartCoroutine(LoadProcess());
+        Debug.Log((float) loadingVideo.clip.length);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator LoadProcess()
+    {
+        yield return new WaitForSeconds(1);
+        videoHolder.SetActive(true);
+        yield return new WaitForSeconds((float)loadingVideo.clip.length);
+        LoadScene(sceneToLoad);
+    }
+}
